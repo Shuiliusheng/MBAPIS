63c2d053839b730587f93620e9680283bd45d61b
Shuiliusheng <cuihongwei@pku.edu.cn>
2023-11-03 22:38:09 +0800

    modified the compile script for the checkpoint loader

--- checkpoint_riscv/restoreCkpt/readckpt/riscv.sh
+++ checkpoint_riscv/restoreCkpt/readckpt/riscv.sh
@@ -6,6 +6,7 @@ target="readckpt_gen.riscv"
 
 filelist=`find . -name "*.cpp" `
 echo ${cc} $filelist $flags -T ./link.lds -o ${target}
+filelist="./takeover_syscall.cpp ./recovery_fload.cpp ./fastlz.cpp ./user_jmp.cpp ./elf_load.cpp ./ckpt_load.cpp ./readckpt.cpp"
 ${cc} $filelist $flags -T ./link.lds -o ${target}
 
 # objdump=riscv64-unknown-linux-gnu-objdump
